d527926c532d4c133cedea6fd51789f665d02e9f
Prashanth <prashanth@example.com>
2026-03-05 16:20:55 +0530

    Fix Vercel deployment: simplify config, add root package.json, fix API CommonJS syntax

--- api/login.js
+++ api/login.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -25,4 +25,4 @@ export default function handler(req, res) {
   } else {
     return res.status(401).json({ message: 'Invalid credentials' });
   }
-}
+};
